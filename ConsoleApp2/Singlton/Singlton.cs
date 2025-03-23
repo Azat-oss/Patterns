f21d4768a0b23dfaa8e71a5a1373b0faefc01ab0
Azat-oss <yuzazat8@gmail.com>
2025-03-23 10:57:58 +0300

    Синглетон ##

---
+++ ConsoleApp2/Singlton/Singlton.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Home_Work.Singlton;
+
+public class DatabaseConnection
+{
+    private static DatabaseConnection database;
+    private DatabaseConnection()
+    {
+
+    }
+
+    public static DatabaseConnection GetInstance()
+    {
+        if (database == null)
+        {
+            database = new DatabaseConnection();
+        }
+
+        return database;
+    }
+
+}
